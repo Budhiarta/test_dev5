8ab2490ce97830915cc1c68e0607dbe3e91360f0
Budhiarta <budhisuputra13@gmail.com>
2023-01-01 23:52:57 +0800

    drop android client

--- app/controllers/api/v1/handleGoogleLoginOrRegister.js
+++ app/controllers/api/v1/handleGoogleLoginOrRegister.js
@@ -4,7 +4,6 @@ const userService = require("../../../services/userService");
 const { OAuth2Client } = require("google-auth-library")
 const { JWT_SECRET_KEY = "Rahasia" } = process.env;
 const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID)
-const client_2 = new OAuth2Client(process.env.ANDROID_CLIENT_ID)
 
 function createToken(user) {
     const payload = {
@@ -42,34 +41,6 @@ async function handleGoogleLoginOrRegister(req, res) {
   } 
 }
 
-async function androHandleGoogleLoginOrRegister(req, res) {
-  const { token } = req.body;
-  
-  try {
-    const ticket = await client_2.verifyIdToken({
-      idToken: token,
-      audience : process.env.ANDROID_CLIENT_ID
-    })
-  
-    const { email,name } = ticket.getPayload();
-    const role = "Member";
-  
-    console.log(ticket.getPayload())
-  
-    let user = await userService.findByEmail(email);
-    if (!user) user = await userService.create({ Email: email, Name: name, Role: role });
-  
-    const accessToken = createToken(user);
-  
-    res.status(201).json({ accessToken });
-  } catch (err) {
-    res.status(401).json({ error: { name: err.name, message: err.message } });
-  } 
-}
 
 
-
-module.exports = {
-handleGoogleLoginOrRegister,
-androHandleGoogleLoginOrRegister
-}
+module.exports = handleGoogleLoginOrRegister;
